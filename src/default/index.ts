ba9f141c964f6aa939af979c1bfc6c006c384b98
shonentropy3 <qwerdfkkt@163.com>
2024-03-27 23:07:47 +0800

    total

--- src/default/index.ts
+++ src/default/index.ts
@@ -1,6 +1,6 @@
 import { Transfer as TransferEvent } from "../../generated/magpie-mmETH/ERC20";
 import { ADDRESS_ZERO } from "../utils/constants";
-import { loadOrCreatePoint, toLowerCase } from "../utils/point";
+import { loadOrCreatePoint, toLowerCase, loadOrCreateTotalPoint } from "../utils/point";
 
 export function handleTransfer(event: TransferEvent, project: string): void {
   const transferShares = event.params.value;
@@ -13,16 +13,22 @@ export function handleTransfer(event: TransferEvent, project: string): void {
   if (from.notEqual(ADDRESS_ZERO)) {
     // burn or transfer to others
     const point = loadOrCreatePoint(from, project);
+    const totalPoint = loadOrCreateTotalPoint(from, project);
     point.timeWeightAmountOut = point.timeWeightAmountOut.plus(increase);
+    totalPoint.totalTimeWeightAmountOut = totalPoint.totalTimeWeightAmountOut.plus(increase);
     point.balance = point.balance.minus(transferShares);
+    totalPoint.totalBalance = totalPoint.totalBalance.minus(transferShares);
     point.save();
   }
 
   if (to.notEqual(ADDRESS_ZERO)) {
     // mint or receive token from others
     const point = loadOrCreatePoint(to, project);
+    const totalPoint = loadOrCreateTotalPoint(from, project);
     point.timeWeightAmountIn = point.timeWeightAmountIn.plus(increase);
+    totalPoint.totalTimeWeightAmountIn = totalPoint.totalTimeWeightAmountIn.plus(increase);
     point.balance = point.balance.plus(transferShares);
+    totalPoint.totalBalance = totalPoint.totalBalance.plus(transferShares);
     point.save();
   }
 }
--- src/utils/point.ts
+++ src/utils/point.ts
@@ -1,4 +1,4 @@
-import { Point } from "../../generated/schema";
+import { Point, TotalPoint } from "../../generated/schema";
 import { Bytes, crypto } from "@graphprotocol/graph-ts";
 import { BIGINT_ZERO } from "./constants";
 
@@ -20,6 +20,25 @@ export function loadOrCreatePoint(address: Bytes, project: string): Point {
     point.save();
   }
   return point;
+}
+
+export function loadOrCreateTotalPoint(address: Bytes, project: string): TotalPoint {
+  address = toLowerCase(address);
+
+  const id = Bytes.fromByteArray(
+    crypto.keccak256(address.concat(Bytes.fromUTF8(project)))
+  );
+  let totalPoint = TotalPoint.load(id);
+
+  if (!totalPoint) {
+    totalPoint = new TotalPoint(id);
+    totalPoint.totalBalance = BIGINT_ZERO;
+    totalPoint.project = project;
+    totalPoint.totalTimeWeightAmountIn = BIGINT_ZERO;
+    totalPoint.totalTimeWeightAmountOut = BIGINT_ZERO;
+    totalPoint.save();
+  }
+  return totalPoint;
 }
 
 export function toLowerCase(address: Bytes): Bytes {
