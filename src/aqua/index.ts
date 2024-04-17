d8c91b0c615a8f65229e64c1fb3385373f141fa6
robert-zklink <robert@zklink.org>
2024-04-17 23:48:49 +0800

    Added renzo and aqua.

---
+++ src/aqua/index.ts
@@ -0,0 +1,6 @@
+import { handleTransfer as defaultHandleTransfer } from "../default/index";
+import { Transfer as TransferEvent } from "../../generated/magpie-mmETH/ERC20";
+
+export function handleTransfer(event: TransferEvent): void {
+  return defaultHandleTransfer(event, "aqua");
+}
